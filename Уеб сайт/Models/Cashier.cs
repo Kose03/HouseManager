bd68c2c807eb886e01e3f848e194abc3c959d8eb
Константин Токов <47472941+Kose03@users.noreply.github.com>
2022-03-03 22:05:41 +0200

    Създаване на модел на базата от данни
    Инсталиране на пакети и създаване на първоначален модел на базата от данни на проекта чрез метода Code First.

---
+++ Уеб сайт/Models/Cashier.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HouseManager.Models
+{
+    public class Cashier
+    {
+        public int Id { get; set; }
+        //public virtual ICollection<User>? Users { get; set; }
+        public virtual User? ClientId { get; set; }
+        public int UserId { get; set; }
+        
+        [Required]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Дължима сума")]
+        public double Sum{ get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Краен срок за плащане")]
+        public DateTime endDate { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Дата на заплащане")]
+        public DateTime payDate { get; set; }
+       
+    }
+}
