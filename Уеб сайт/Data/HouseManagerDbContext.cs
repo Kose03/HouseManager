4929ab24c5c146e81d29535f3b30eed7f57acc99
Константин Токов <47472941+Kose03@users.noreply.github.com>
2022-03-03 22:03:09 +0200

    Създаване на модел на базата от данни
    Инсталиране на пакети и създаване на първоначален модел на базата от данни на проекта чрез метода Code First.

---
+++ Уеб сайт/Data/HouseManagerDbContext.cs
@@ -0,0 +1,31 @@
+﻿using HouseManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseManager.Data
+{
+    public class HouseManagerDbContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Cashier> Cashiers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=HouseholdManager;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            optionsBuilder.UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Cashier>()
+                .HasOne(o => o.ClientId)
+                .WithMany(p => p.Users)
+                .HasForeignKey(o => o.UserId);
+            modelBuilder.Entity<User>()
+                .HasMany(p => p.Users)
+                .WithOne(o => o.ClientId)
+                .HasForeignKey(o => o.UserId);
+
+
+        }
+    }
+}
---
+++ Уеб сайт/Models/User.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HouseManager.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Потребителско име")]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Име")]
+        public string FirstName { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Презиме")]
+        public string FathersName { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Фамилия")]
+        public string FamilyName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Парола")]
+        public string Password { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Имейл адрес")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Адрес")]
+        public string Address { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Телефон за контакт")]
+        public int TelephoneNumber { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Членове на домакинството")]
+        public string HouseHoldMembers { get; set; }
+        public string Role { get; set; }
+        public virtual ICollection<Cashier>? Users { get; set; }
+
+    }
+}
